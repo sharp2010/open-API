670ab18c8bde8d055edefcc9cf51b4d6993da9d3
Tracy Sharp <tracysharp@Tracys-MacBook-Pro.local>
2025-02-08 12:06:17 -0500

    changed code around to hide image until clicked

--- js/index.js
+++ js/index.js
@@ -2,14 +2,14 @@
 const apiKey = 'live_GOHQq1ThqJOgEMotKUIE78KMv5yI2VmGgNzeY4IsyLwsLpoudVDx4X7dlcaZxqhg';
 
 // function to make GET request with headers
-const fetchData= async (url) => {
+const fetchData = async (url) => {
     try {
         const response = await axios.get(url, {
             // header is need to authenticate the request
-            headers: {'x-api-key': apiKey},
+            headers: { 'x-api-key': apiKey },
         });
         return response.data;
-    }  catch (error) {
+    } catch (error) {
         console.error('Error getting data:', error);
     }
 };
@@ -32,24 +32,26 @@ const displayBreeds = (breeds) => {
     });
 };
 
-    // function to get cat image
-    const getCatImage = async () => {
-        const url = 'https://api.thecatapi.com/v1/images/search';
-        const data = await fetchData(url); 
+// function to get cat image
+const getCatImage = async () => {
+    const url = 'https://api.thecatapi.com/v1/images/search';
+    const data = await fetchData(url);
 
     //get the image url from the response
-        const catImageUrl = data[0].url;
+    const catImageUrl = data[0].url;
 
     // update src of image element
-        document.getElementById('catImage').src = catImageUrl;
-        console.log('Cat Image URL:', catImageUrl);
+    document.getElementById('catImage').src = catImageUrl;
+    console.log('Cat Image URL:', catImageUrl);
+    catSection.style.display = 'block';
 };
-       
+
 //function to get cat breeds
 const getCatBreeds = async () => {
-const breeds = await fetchData('https://api.thecatapi.com/v1/breeds');
-console.log(breeds);
+    const breeds = await fetchData('https://api.thecatapi.com/v1/breeds');
+    console.log(breeds);
     displayBreeds(breeds);
+    breedsSection.style.display = 'block';
 };
 
 // function to search breeds
@@ -59,22 +61,22 @@ const searchBreed = async () => {
     const filteredBreeds = breeds.filter(breed => breed.name.toLowerCase().includes(searchName));
     displayBreeds(filteredBreeds);
 };
-        
+
 // Event listeners for navigation links
 const catLink = document.getElementById('catLink');
 const breedsLink = document.getElementById('breedsLink');
 const catSection = document.getElementById('cat');
 const breedsSection = document.getElementById('breeds');
 
+catSection.style.display = 'none';
+breedsSection.style.display = 'none';
+
 catLink.addEventListener('click', () => {
-    catSection.style.display = 'block';
-  //  breedsSection.style.display = 'none';
     getCatImage();
 });
 
 breedsLink.addEventListener('click', () => {
-    catSection.style.display = 'block';
-  // breedsSection.style.display = 'none';
+
     getCatBreeds();
 });
 
